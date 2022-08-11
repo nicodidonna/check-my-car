b3f61bc93439fcdc37d4d6ef067db896430e4c9f
Nico Didonna <nico_didonna@yahoo.it>
2022-08-11 17:05:15 +0200

    Page parco auto

--- src/app/parco-auto/parco-auto.component.ts
+++ src/app/parco-auto/parco-auto.component.ts
@@ -12,4 +12,26 @@ export class ParcoAutoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  auto1 = {
+    marca: "Dacia",
+    modello: "Duster",
+    targa: "FA773ZR",
+    cilindrata: 1.5,
+    cavalli: 115,
+    annoImmatricolazione: 2015,
+    alimentazione: "Diesel"
+  }
+
+  auto2 = {
+    marca: "Citroen",
+    modello: "C3",
+    targa: "CK829YZ",
+    cilindrata: 1.1,
+    cavalli: 60,
+    annoImmatricolazione: 2004,
+    alimentazione: "Benzina"
+  }
+
+  arrAuto = [this.auto1,this.auto2];
 }
