0561aac58d874af38daf4a5d910d666e791dacc0
Nico Didonna <nico_didonna@yahoo.it>
2022-08-23 16:38:40 +0200

    Revisione Service update

--- src/app/@revisione/services/revisione-service.service.ts
+++ src/app/@revisione/services/revisione-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Revisione } from '../class/RevisioneClass';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RevisioneServiceService {
 
+  listaRevisioni : Array<Revisione> = [];
+
   constructor() { }
+
+  getRevisioni(){
+    return this.listaRevisioni;
+  }
+
+  aggiungiRevisione(revisione){
+    this.listaRevisioni.push(revisione);
+  }
+
+  revisioneFilter(targa){
+      return this.getRevisioni().filter(
+       (tagliando) => {tagliando.auto.targa == targa});
+  }
 }
