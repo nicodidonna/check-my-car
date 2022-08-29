1e4609b0e9115f354402826ac0837bab317371ca
Nico Didonna <nico_didonna@yahoo.it>
2022-08-29 17:45:02 +0200

    Cards Manutenzione

--- src/app/manutenzione-straordinaria/manutenzione-straordinaria.component.ts
+++ src/app/manutenzione-straordinaria/manutenzione-straordinaria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutoServiceService } from '../@auto/services/auto-service.service';
 
 @Component({
   selector: 'app-manutenzione-straordinaria',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManutenzioneStraordinariaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public autoService : AutoServiceService) { }
 
   ngOnInit(): void {
   }
