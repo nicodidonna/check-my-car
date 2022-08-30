3ff5414c90cacbe763b9f36e65a9832a82705805
Nico Didonna <nico_didonna@yahoo.it>
2022-08-30 16:30:37 +0200

    Modale manutenzione

--- src/app/manutenzione-straordinaria/manutenzione-straordinaria.component.ts
+++ src/app/manutenzione-straordinaria/manutenzione-straordinaria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { AutoServiceService } from '../@auto/services/auto-service.service';
 
 @Component({
@@ -9,6 +10,8 @@ import { AutoServiceService } from '../@auto/services/auto-service.service';
 export class ManutenzioneStraordinariaComponent implements OnInit {
 
   constructor(public autoService : AutoServiceService) { }
+
+  form : FormGroup;
 
   ngOnInit(): void {
   }
