fc1ad5e15870f4c8a09059f7655ce8dc9543f79c
Nico Didonna <nico_didonna@yahoo.it>
2022-09-09 17:48:17 +0200

    Add tagliandi to DB

--- src/app/@tagliando/services/tagliando-service.service.ts
+++ src/app/@tagliando/services/tagliando-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Tagliando } from '../class/TagliandoClass';
 
 @Injectable({
@@ -8,7 +9,7 @@ export class TagliandoServiceService {
 
   listaTagliandi: Array<Tagliando> = [];
 
-  constructor() { }
+  constructor(private database : AngularFirestore) { }
 
   getTagliandi(){
     return this.listaTagliandi;
@@ -16,6 +17,7 @@ export class TagliandoServiceService {
 
   aggiungiTagliando(tagliando){
     this.listaTagliandi.push(tagliando);
+    return this.database.collection('Tagliandi').add(JSON.parse(JSON.stringify(tagliando))).then(res=>{},err=>{});
   }
 
   tagliandiFilter(targa){
