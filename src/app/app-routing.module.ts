d11ce19bab50db8f43674313c16e3eefaded0e17
Nico Didonna <nico_didonna@yahoo.it>
2022-10-29 19:41:06 +0200

    firebase first deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {path:"parco-auto", loadChildren: ()=>import("./parco-auto/parco-auto.module").then(m=>m.ParcoAutoModule)},
   {path:"revisioni",loadChildren:()=>import("./revisioni/revisioni.module").then(m=>m.RevisioniModule)},
   {path:"tagliandi",loadChildren:()=>import("./tagliandi/tagliandi.module").then(m=>m.TagliandiModule)},
-  {path:"assicurazione-bollo",loadChildren:()=>import("./assicurazione-bollo/assicurazione-bollo.module").then(m=>m.AssicurazioneBolloModule)},
+  {path:"assicurazione",loadChildren:()=>import("./assicurazione-bollo/assicurazione-bollo.module").then(m=>m.AssicurazioneBolloModule)},
   {path:"manutenzione-straordinaria",loadChildren:()=>import("./manutenzione-straordinaria/manutenzione-straordinaria.module").then(m=>m.ManutenzioneStraordinariaModule)}
 ];
 
